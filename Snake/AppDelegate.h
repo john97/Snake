74452e00d4c226ea7759cf22bb37aade636498bb
john97 <keshavgoyal9697@gmail.com>
2017-05-26 14:30:54 +0530

    Initial Commit

---
+++ Snake/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Snake
+//
+//  Created by Keshav Goyal on 26/05/17.
+//  Copyright © 2017 Keshav Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Snake/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Snake
+//
+//  Created by Keshav Goyal on 26/05/17.
+//  Copyright © 2017 Keshav Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
